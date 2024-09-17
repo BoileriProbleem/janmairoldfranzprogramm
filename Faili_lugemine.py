6e04aae7eebe7a03652e5a43f46d0e6d83b2d8a4
Maister51 <152634217+Maister51@users.noreply.github.com>
2024-09-17 12:50:27 +0300

    Add files via upload

--- Faili_lugemine.py
+++ Faili_lugemine.py
@@ -1,11 +1,22 @@
+import json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 url = "https://www.rimi.ee/epood/ee/tooted/alkohol/c/SH-1?currentPage=1&pageSize=80&query=%3Arelevance%3AallCategories%3ASH-1%3AassortmentStatus%3AinAssortment"
 page = urlopen(url)
 html = page.read().decode("utf-8")
-Prisma_olu = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(html, "html.parser")
 olud = open("Olud.txt", "w", encoding="utf-8")
-olud.write(Prisma_olu.get_text())
+test = open("Test.txt", "w", encoding="utf-8")
+kogu_tekst = soup.get_text()
+data = soup.find('script', type='text/plain').get_text()
+test.write(data)
+for rida in data:
+    if rida.strip():
+        olud.write(rida)
+    elif rida == " ":
+        olud.write(rida)
+    elif rida == "\t":
+        olud.write(rida)
 olud.close()
 hinnad_olu = open("hinnad_olu.txt", "w")
