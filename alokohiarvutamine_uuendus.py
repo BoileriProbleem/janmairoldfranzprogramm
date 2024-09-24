37f6bf6c59f698afdb7e55e479a841581dad6c7b
Franzstevensaarm <162054959+Franzstevensaarm@users.noreply.github.com>
2024-09-24 12:51:15 +0300

    Add files via upload

---
+++ alokohiarvutamine_uuendus.py
@@ -0,0 +1,88 @@
+import re
+#\u00d5 = "Õ"
+
+def loeb_failist(failinimi):
+    joogid = []
+    with open(failinimi, "r" , encoding="UTF-8") as fail:
+        while True:
+            nimerida = file.readline().strip()
+            if not nimerida:
+                break
+            hinnarida = file.readline().strip()
+            
+            jooginimi = re.search(r'"name": "(.*?), (\d+)%vol, (\d+)ml"', nimerida)
+            hinnasumma = re.search(r'"price": (\d+\.?\d*)', hinnarida)
+            
+            if jooginimi and hinnasumma:
+                nimi = name_match.group(1)
+                etanool = float(jooginimi.group(2))
+                kogus = float(jooginimi.group(3))
+                hind = float(hinnasumma.group(1))
+                
+                joogid.append({
+                    "nimi": nimi,
+                    "etanool": etanool,
+                    "kogus": kogus,
+                    "hind": hind
+                    })
+    return joogid
+            
+       # reader = csv.DictReader(file)
+       # for row in rider
+        #    joogid.append({
+           #     "nimi" : row["nimi"],
+           #     "etanool" : float(row["etanool"]),
+            #    "hind" : float(row["hind"]),
+            #    "kogus" : float(row["kogus"])
+            #    })
+         #   return joogid
+
+def soovitud_andmed():
+    min_etanool = float(input("sisesta enda minimaalne etanooli soovitud kogus (ml): "))
+    max_etanool = float(input("sisesta enda maksimaalne etanooli soovitud kogus (ml): "))
+    min_kogus = float(input("sisesta enda minimaalne joogi suurus (ml): "))
+    max_kogus = float(input("sisesta enda maksimaalne joogi suurus (ml): "))
+    min_hind = float(input("sisesta enda minimaalne hind (€): "))
+    max_hind = float(input("sisesta enda maksimaalne hind (€): "))
+    
+    return min_etanool, max_etanool, min_kogus, max_kogus, min_hind, max_hind
+    
+
+            
+def parimjoogivalik(joogid, min_etanool, max_etanool, min_kogus, max_kogus, min_hind, max_hind):
+    sobiv_jook = []
+    
+    for jook in joogid:
+        if (min_etanool <= jook["etanool"] <= max_etanool and
+            min_kogus <= jook["kogus"] <= max_kogus and
+            min_hind <= jook["hind"] <= max_hind):
+            sobiv_jook.append(jook)
+    return sobiv_jook
+            
+def etanooli_arvutus_kogus(jook):
+    etanooli_ml = (jook["etanool"] / 100) * jook["kogus"]
+    return etanooli_ml
+
+def etanooli_arvutus_hind(jook):
+    etanooli_ml = etanooli_arvutus_kogus(jook)
+    etanooli_hind_ml_suhtes = jook["hind"] / etanooli_ml
+    return etanooli_hind_ml_suhtes
+
+if __name__ == "__main__":
+    
+    joogid = loe_failist("joogid.txt")
+    min_etanool, max_etanool, min_kogus, max_kogus, min_hind, max_hind, soovitud_andmed()
+    sobiv_jook = parimjoogivalik(joogid, min_etanool, max_etanool, min_kogus, max_kogus, min_hind, max_hind)
+    
+    if sobiv_jook:
+        print("sobib jook: ")
+        for jook in sobiv_jook:
+            etanooli_hind = etanooli_arvutus_hind(jook)
+            print(f"Nimi: {jook['nimi']}, hind: {jook['hind']} €, Etanool: {jook['etanool']} %, Kogus: {jook['kogus']} ml")
+            print(f"Etanooli hind (1ml kohta): {etanooli_hind:.4f} €")
+    else:
+        print("Ei leidnud teile soovitud jooki")
+    
+
+
+
