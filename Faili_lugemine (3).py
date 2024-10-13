1a1bc3291db758c31d9bedb9a60f4588423fadb3
Maister51 <152634217+Maister51@users.noreply.github.com>
2024-10-13 19:38:55 +0300

    Add files via upload

---
+++ Faili_lugemine (3).py
@@ -0,0 +1,81 @@
+import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+def andmesaak(test, kokku, eelmineväli, eelmine):
+    olud = open("Olud.txt", "a", encoding="utf-8")
+    kakskoos = ""
+    for rida in test:
+        if rida[0].isdigit():
+            if kokku == False:
+                kakskoos = eelmine + "," + rida
+                kokku = True
+            else:
+                olud.write(kakskoos + "," + rida)
+                kokku = False
+                eelmineväli = False
+        elif eelmineväli:
+            if kakskoos.find(eelmine) != -1:
+                olud.write(kakskoos)
+            else:
+                olud.write(eelmine)
+            eelmineväli = False
+            kokku = False
+        if rida.find("name") != -1 or rida.find("price") != -1:
+            eelmine = rida
+            eelmineväli = True
+    olud.close()
+            
+def leht(i):
+    url = "https://www.rimi.ee/epood/ee/tooted/alkohol/c/SH-1?currentPage=" + str(i) + "&pageSize=80&query=%3Arelevance%3AallCategories%3ASH-1%3AassortmentStatus%3AinAssortment"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    test = open("Test.txt", "w", encoding="utf-8")
+    data = soup.find('script', type='text/plain').get_text()
+    test.write(data)
+    test.close()
+    with open("Test.txt", "r", encoding='utf-8') as f:
+        test = f.read()
+    f.close()
+    test = test.split(",")
+    return test
+
+def tekst():
+    with open("Olud.txt", "r", encoding="utf-8") as yeah:
+        tekst = yeah.read().replace(r'\/', '/')
+        tekst = tekst.replace("\u015b", "ś")
+        decoded_tekst = tekst.encode('utf-8').decode('unicode-escape')
+    
+    with open("Lahja.txt", "w", encoding="utf-8") as lahja:
+        lahja.write(decoded_tekst)
+    
+    
+
+i = 1
+olud = open("Olud.txt", "w", encoding="utf-8")
+olud.close()
+lru = "https://www.rimi.ee/epood/ee/tooted/alkohol/c/SH-1?currentPage=" + str(i) + "&pageSize=80&query=%3Arelevance%3AallCategories%3ASH-1%3AassortmentStatus%3AinAssortment"
+egap = urlopen(lru)
+lmth = egap.read().decode("utf-8")
+puos = BeautifulSoup(lmth, "html.parser")
+võhandu = open("Test.txt", "w", encoding="utf-8")
+pagination_items = puos.find_all('li', class_='pagination__item')
+numbriots = re.compile(r'\d+')
+esimene = True
+for rida in pagination_items:
+    if '-chevron' in str(rida):
+        if not esimene:
+            number = numbriots.search(str(eelmine))
+            if number:
+                maks = number.group()
+        esimene = False
+    eelmine = rida
+while i <= int(maks):
+    tagastus = leht(i)
+    i += 1
+    Gojo = "jap"
+    Filip = False
+    Hamlet = False
+    andmesaak(tagastus, Hamlet, Filip, Gojo)
+tekst()
