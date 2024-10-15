beb455f55ec1e312ba142db48ef683e6c07638b0
Franzstevensaarm <162054959+Franzstevensaarm@users.noreply.github.com>
2024-10-15 12:57:16 +0300

    Add files via upload
    Update

--- franz_valmis.py
+++ franz_valmis.py
@@ -12,21 +12,30 @@ def loeb_failist(failinimi):
             hinnarida = fail.readline().strip()
 
             
-            jooginimi = re.search(r'"name": "(.*?)\s(\d+,\d+%)\s(\d+(?:,\d+)?)[lL]', nimerida)
+            jooginimi = re.search(r'"name": "(.*?)\s(\d+%)\s(\d+(?:,\d+)?)[lL]', nimerida) or re.search(r'"name": "(.*?)\s(\d+,\d+%vol)\s(\d+(?:,\d+)?)[lL]' , nimerida) or re.search(r'"name": "(.*?)\s(\d+%vol?)\s(\d+(?:,\d+)?)[lL]', nimerida)
             hinnasumma = re.search(r'"price": (\d+\.?\d*)', hinnarida)
 
             if jooginimi and hinnasumma:
-                nimi = jooginimi.group(1)  
-                etanool = float(jooginimi.group(2).replace(',', '.').strip('%')) 
-                kogus = float(jooginimi.group(3).replace(',', '.'))  
-                hind = float(hinnasumma.group(1))  
+                    nimi = jooginimi.group(1)  
+                    etanool = float(jooginimi.group(2).replace(',', '.').strip('%vol').strip("%"))
 
-                joogid.append({
-                    "nimi": nimi,
-                    "etanool": etanool,
-                    "kogus": kogus,
-                    "hind": hind
-                })
+                    # Check for "-pakk" and extract the quantity
+                    match = re.search(r'(\d+)-pakk"', nimerida)
+                    if match:
+                        kogus = float(match.group(1)) * float(jooginimi.group(3).replace(',', '.'))
+                    else:
+                        kogus = float(jooginimi.group(3).replace(',', '.'))  
+
+                    hind = float(hinnasumma.group(1))
+
+                    joogid.append ({
+                        "nimi": nimi,
+                        "etanool": etanool,
+                        "kogus": kogus,
+                        "hind": hind
+                    })
+            else:
+                print(nimerida)
 
     return joogid
 
@@ -64,7 +73,7 @@ def etanooli_arvutus_hind(jook):
     return etanooli_hind_ml_suhtes
 
 if __name__ == "__main__":
-    joogid = loeb_failist("Olud.txt")
+    joogid = loeb_failist("Lahja.txt")
     parim_jook = parimjoogivalik(joogid, min_etanool, max_etanool, min_kogus, max_kogus, min_hind, max_hind)
 
     if parim_jook:
